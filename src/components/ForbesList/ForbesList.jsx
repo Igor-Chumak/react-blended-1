73249843e8c09173953a0ac7ddbac462528a91b4
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-07-23 15:50:20 +0200

    t3

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -7,7 +7,37 @@ import {
   TitleBottom,
   LeaderBoardProfiles,
 } from './ForbesList.styled';
+import PropTypes from 'prop-types';
 
-export const ForbesList = () => {
-  return <div>ForbesList</div>;
+export const ForbesList = ({ forbes }) => {
+  return (
+    <LeaderBoard>
+      <BoardHeader>
+        <BoardTitle>
+          <TitleTop>Forbes</TitleTop>
+          <TitleBottom>Leader board</TitleBottom>
+        </BoardTitle>
+      </BoardHeader>
+
+      <LeaderBoardProfiles>
+        {forbes.map(({ id, name, capital, avatar, isIncrease }) => (
+          <ForbesListItem
+            key={id}
+            name={name}
+            capital={capital}
+            avatar={avatar}
+            isIncrease={isIncrease}
+          />
+        ))}
+      </LeaderBoardProfiles>
+    </LeaderBoard>
+  );
+};
+
+ForbesList.propTypes = {
+  forbes: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  ),
 };
