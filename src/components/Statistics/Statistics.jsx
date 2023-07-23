480427cb19e396cb69b212ae5ebb928e3eed192c
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-23 16:22:38 +0300

    create Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,11 +3,41 @@ import { StatisticsList, StatisticTitle } from './Statistics.styled';
 import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
+import PropTypes from 'prop-types';
 
-export const Statistics = () => {
+const icons = [<FaRegThumbsUp />, <MdPeople />, <MdOutlineProductionQuantityLimits />, <GiTreeDoor />];
+
+export const Statistics = ({title, stats}) => {
   return (
     <>
-      <div>StatisticsList</div>
+      <div>
+        {title && <StatisticTitle>{title}</StatisticTitle>}
+        <StatisticsList>
+          {
+            stats.map((statsItem, statsIndex) => 
+              <StatisticItem
+                key={statsItem.id}
+                icon={icons[statsIndex]}
+                title={statsItem.title}
+                total={statsItem.total}
+              />
+            )
+          }
+        </StatisticsList>
+      </div>
     </>
   );
 };
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        total: PropTypes.number.isRequired
+      }
+    )
+  ).isRequired
+}
